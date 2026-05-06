d9f864092db9ac6ee9c1cf5270d8a90fb8f3edd9
pradip <pradipsubedi360@gmail.com>
2026-05-06 16:08:05 +0545

    test: run smoke test-suite parallely

--- tests/e2e-playwright/steps/ui/resources.ts
+++ tests/e2e-playwright/steps/ui/resources.ts
@@ -1078,7 +1078,7 @@ export async function userShouldSeeActivityOfResources({
   for (const info of resources) {
     await resourceObject.checkActivity({
       resource: info.resource,
-      activity: substitute(info.activity)
+      activity: substitute(info.activity, world)
     })
   }
 }
